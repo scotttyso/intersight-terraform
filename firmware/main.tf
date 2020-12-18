0d45a3aca01ce147414cfa1b887107f5c4f63ce0
scotttyso <tyscott@cisco.com>
2020-12-18 10:25:19 -0500

    modified firmware main.tf

---
+++ firmware/main.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_version = "~> 0.13"
+    required_providers {
+        intersight = {
+            source = "CiscoDevNet/intersight"
+            version = "0.1.1"
+        }
+    }
+}
+
+provider "intersight" {
+    apikey = var.api_key
+    secretkeyfile = var.secretkeyfile
+    endpoint = var.endpoint
+}
+
+## Get the MO data for the default organization in the account.
+# /api/v1/organization/Organizations$filter=(Name eq 'default')
+data "intersight_organization_organization" "default" {
+    name = var.organization
+}
+
+data "intersight_compute_blade" "default" {
+    serial = var.serial
+}
+
+output "org_default_moid" {
+    value = data.intersight_organization_organization.default.moid
+}
