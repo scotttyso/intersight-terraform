d13782faa3fd934188495787c1ff8f182bc9e04c
scotttyso <tyscott@cisco.com>
2020-12-18 10:16:29 -0500

    adding firmware directory

---
+++ firmware/firmware_upgrade_direct.tf
@@ -0,0 +1,54 @@
+resource "intersight_firmware_upgrade" "sprint-firmware-update" {
+  /*
+  Description: "Desired upgrade mode to choose either direct download based upgrade or network share upgrade."
+  ~ `direct_upgrade` - Upgrade mode is direct download.
+  ~ `network_upgrade` - Upgrade mode is network upgrade.
+  */
+  upgrade_type =  "direct_upgrade"
+
+  direct_download {
+    /*
+     Description: "Source type referring the image to be downloaded from CCO or from a local HTTPS server.
+    ~ `cisco` - Image to be downloaded from Cisco software repository.
+    ~ `localHttp` - Image to be downloaded from a https server."
+    */
+    image_source = "cisco"
+
+    /* 
+    Description: "Option to control the upgrade, e.g., sd_upgrade_mount_only - download the image into sd and upgrade wait for the server on-next boot.
+     ~ `sd_upgrade_mount_only` - Direct upgrade SD upgrade mount only.
+     ~ `sd_download_only` - Direct upgrade SD download only.
+     ~ `sd_upgrade_only` - Direct upgrade SD upgrade only.
+     ~ `sd_upgrade_full` - Direct upgrade SD upgrade full.
+     ~ `upgrade_full` - The upgrade downloads or mounts the image, and reboots immediately for an upgrade.
+     ~ `upgrade_mount_only` - The upgrade downloads or mounts the image. The upgrade happens in next reboot.
+     ~ `chassis_upgrade_full` - Direct upgrade chassis upgrade full." 
+     */
+    upgrade_option = "upgrade_full"
+
+    /*
+    http_server {
+        location_link = "http://10.5.30.39/staticfiles/ucs-c240m5-huu-4.1.1f.iso"
+    }
+    */
+  }
+
+  distributable {
+    /* 
+    Description.  What Image is to be used for the upgrade when doing direct_download with cisco.com aka intersight.com
+    ~ * moid of the file, pulled from intersight API
+    ~ * selector for the name of the file
+    ~ Only need one or the other.  Comment out whichever is unused
+    */
+    # moid = "5f90656b6567612d3184425e"
+    selector = "$filter(Name eq '${var.image}')"
+  }
+
+  # skip_estimate_impact {}
+
+  server {
+    # Server Type is either compute.Blade or compute.RackUnit
+    object_type = var.object_type
+    moid = data.intersight_compute_blade.default.moid
+  }
+}
---
+++ firmware/variables.tf
@@ -0,0 +1,45 @@
+# https://intersight.com/an/settings/api-keys/
+# Generate API key to obtain the API Key and PEM file
+
+variable "api_key" {
+    description = "API Key for Terraform Demo Account"
+    type        = string
+}
+
+variable "endpoint" {
+    description = "Intersight API endpoint"
+    type        = string
+    default     = "https://intersight.com"
+}
+
+variable "image" {
+    description = "Define the image to use"
+    type        = string
+    default   = "ucs-intersight-server-b200-m5.4.1.2a.gbin"
+}
+
+variable "object_type" {
+    description = "Define if Blade or Rackmount"
+    type        = string
+    default   = "compute.Blade"
+    # default     = "compute.RackUnit"
+}
+
+variable "organization" {
+    description = "Which Organization in Intersight"
+    type        = string
+    default   = "IMM"
+}
+
+
+variable "serial" {
+    description = "Define the Serial Number of the Server"
+    type        = string
+    default   = "FCH21427CHB"
+}
+
+variable "secretkeyfile" {
+    description = "Filename (PEM Format) that provides secret key for Terraform Account"
+    type        = string
+    default     = "/home/tyscott/SecretKey.txt"
+}
